93f809a67a4329295fc60c7a565f9bcdb4cc02a2
xianxiong <xianxiong.sun@fraudmetrix.cn>
2017-06-08 12:14:24 +0800

    radioButton

--- components/radio/radioButton.js
+++ components/radio/radioButton.js
@@ -1,3 +1,39 @@
 /**
  * Created by sunxianxiong on 17/6/8.
  */
+/* eslint-disable no-unused-vars */
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import cn from 'classnames';
+import Radio from './radio';
+import s from './style';
+
+export default class RadioButton extends Component {
+  static defaultProps = {
+    prefixCls: `${s.radioPrefix}-button`
+  };
+
+  static contextTypes = {
+    radioGroup: PropTypes.object
+  };
+
+  render() {
+    const {props, context} = this;
+
+    const {radioGroup} = context;
+
+    const radioPropTypes = {...props};
+
+    if (radioGroup) {
+      radioPropTypes.onChange = radioGroup.onChange;
+      radioPropTypes.disabled = radioPropTypes.disabled || radioGroup.disabled;
+      radioPropTypes.checked = radioPropTypes.value === radioGroup.value;
+    }
+
+    return (
+        <Radio
+          {...radioPropTypes}
+        />
+    );
+  }
+}
