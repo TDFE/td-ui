a0c50cdbdcbda3cd36306363653b3cf4c9c6a382
can.yang <can.yang@sc.10086.cn.defaultbadlist>
2017-06-14 19:52:34 +0800

    实现carousel

---
+++ components/carousel/indicator-dots.js
@@ -0,0 +1,52 @@
+/* eslint-disable */
+import React from 'react'
+// import PropTypes from 'prop-types'
+
+function Dot (props) {
+  return (
+    <span onClick={((e)=>{
+        const index = props.index;
+        return (e)=>{
+          props.onClick(index);
+        }
+      })()}
+      style={{
+      display: 'inline-block',
+      height: '8px',
+      width: '8px',
+      borderRadius: '4px',
+      backgroundColor: 'white',
+      margin: '7px 5px',
+      opacity: props.selected ? '1' : '0.3',
+      transitionDuration: '300ms',
+      cursor: 'pointer'
+    }} />
+  )
+}
+
+export default function IndicatorDots (props) {
+  const wrapperStyle = {
+    position: 'absolute',
+    width: '100%',
+    zIndex: '100',
+    bottom: '0px',
+    textAlign: 'center'
+  }
+  // Hide dots when there is only one dot.
+  if (props.total < 2) {
+    return <div style={wrapperStyle} />
+  } else {
+    return (
+      <div style={wrapperStyle}>{
+        Array.apply(null, Array(props.total)).map((x, i) => {
+          return <Dot key={i} index={i} onClick={props.onClick} selected={props.index === i} />
+        })
+      }</div>
+    )
+  }
+}
+
+// IndicatorDots.propTypes = {
+//   index: PropTypes.number.isRequired,
+//   total: PropTypes.number.isRequired
+// }
